fcc3fd86e05b18c213119f11439dfdf60ce7d32d
Russell Selph <rselph@gmail.com>
2018-02-14 08:17:54 -0800

    Finished! (For now.)

--- flickrate.go
+++ flickrate.go
@@ -11,11 +11,14 @@ import (
 	"net/url"
 	"os/user"
 	"path/filepath"
+	"sort"
+	"time"
 )
 
 const (
-	configFile   = ".flickrate"
-	restEndpoint = "https://api.flickr.com/services/rest/"
+	configFile    = ".flickrate"
+	restEndpoint  = "https://api.flickr.com/services/rest/"
+	secondsPerDay = 60 * 60 * 24
 )
 
 var (
@@ -24,8 +27,10 @@ var (
 	apisecret string
 
 	targetName string
-	minDays    int
-	minViews   int
+	minDays    int64
+	maxDays    int64
+	minViews   int64
+	show       int
 
 	verbose bool
 	refresh bool
@@ -46,8 +51,10 @@ func main() {
 	flag.StringVar(&authName, "user", "", "your flickr user name")
 	flag.StringVar(&apikey, "key", "", "flickr API key")
 	flag.StringVar(&apisecret, "secret", "", "flickr API secret")
-	flag.IntVar(&minDays, "days", 10, "minimum days")
-	flag.IntVar(&minViews, "views", 1000, "mimimum views")
+	flag.Int64Var(&minDays, "mindays", 90, "minimum age in days")
+	flag.Int64Var(&maxDays, "maxdays", 3650, "maximum age in days")
+	flag.Int64Var(&minViews, "minviews", 1000, "mimimum views")
+	flag.IntVar(&show, "top", 10, "show top n photos")
 	flag.BoolVar(&verbose, "v", false, "verbose")
 	flag.BoolVar(&refresh, "refresh", false, "refresh login credentials")
 	flag.Parse()
@@ -140,7 +147,13 @@ func doTheThing() {
 
 	photos := getPhotos(userId)
 
-	getDetails(photos)
+	allPhotos := getDetails(photos)
+
+	somePhotos := filterPhotos(allPhotos)
+
+	sortPhotos(somePhotos)
+
+	printPhotos(somePhotos)
 }
 
 type testUserResponse struct {
@@ -233,6 +246,9 @@ func getPhotos(userId string) []photoPtr {
 type photoInfo struct {
 	Id     string `xml:"id,attr"`
 	Secret string `xml:"secret,attr"`
+	Views  int64  `xml:"views,attr"`
+	rate   float64
+	age    int64
 	Dates  struct {
 		Posted           int64  `xml:"posted,attr"`
 		Taken            string `xml:"taken,attr"`
@@ -252,6 +268,7 @@ type getInfoResponse struct {
 }
 
 func getDetails(photos []photoPtr) []*photoInfo {
+	now := time.Now().Unix()
 	infos := make([]*photoInfo, len(photos))
 	for i, photo := range photos {
 		q := flickrQuery{"photo_id": photo.Id}
@@ -260,10 +277,56 @@ func getDetails(photos []photoPtr) []*photoInfo {
 		if err != nil {
 			log.Fatal(err)
 		}
+		if info.Photo.Views != 0 {
+			info.Photo.age = now - info.Photo.Dates.Posted
+			info.Photo.rate = float64(info.Photo.Views) / float64(info.Photo.age)
+		}
 		infos[i] = &info.Photo
+		if i != 0 && i%80 == 0 {
+			fmt.Println()
+		}
+		fmt.Print(".")
 	}
 
+	fmt.Println()
 	return infos
+}
+
+func filterPhotos(photos []*photoInfo) []*photoInfo {
+	filtered := make([]*photoInfo, 0, len(photos))
+	for _, p := range photos {
+		if p.age/secondsPerDay >= minDays && p.age/secondsPerDay <= maxDays && p.Views >= minViews {
+			filtered = append(filtered, p)
+		}
+	}
+	return filtered
+}
+
+type photoList []*photoInfo
+
+func (a photoList) Len() int {
+	return len(a)
+}
+func (a photoList) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+func (a photoList) Less(i, j int) bool {
+	return a[i].rate > a[j].rate
+}
+
+func sortPhotos(photos []*photoInfo) {
+	sort.Stable(photoList(photos))
+}
+
+func printPhotos(photos []*photoInfo) {
+	for i := 0; i < show && i < len(photos); i++ {
+		p := *photos[i]
+		fmt.Printf("%s\t% 5d\t%.3e\t%s\n",
+			time.Unix(p.Dates.Posted, 0).String(),
+			p.Views,
+			p.rate*secondsPerDay,
+			p.Urls.Values[0].Value)
+	}
 }
 
 type flickrQuery map[string]string
@@ -288,7 +351,6 @@ func (fq *flickrQuery) Execute(method string, result interface{}) error {
 		q.Set(k, v)
 	}
 	if config.OauthTokenSecret != "" {
-		// TODO: This signature isn't working
 		authSign(addr, q, config.OauthTokenSecret)
 	} else {
 		addr.RawQuery = q.Encode()
