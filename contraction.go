ada8215d73ff52d07eb58741c3543afa86f16fdb
Russell Selph <dev@selph.net>
2019-05-28 07:56:26 -0700

    Proper UTF8 handling when contracting titles

--- contraction.go
+++ contraction.go
@@ -1,15 +1,22 @@
 package main
 
+import "strings"
+
 var (
-	elipsis = "..."
-	e       = len(elipsis)
+	elipsis = "…"
+	e       = len(strings.Split(elipsis, ""))
 )
 
 func Contract(in string, total, suffix int) string {
-	i := len(in)
+	inUnicode := strings.Split(in, "")
+	i := len(inUnicode)
 	if i <= total {
 		return in
 	}
 
-	return in[:total-suffix-e] + elipsis + in[i-suffix:]
+	outUnicode := inUnicode[:total-suffix-e]
+	outUnicode = append(outUnicode, elipsis)
+	outUnicode = append(outUnicode, in[i-suffix:])
+	return strings.Join(outUnicode, "")
+	//return in[:total-suffix-e] + elipsis + in[i-suffix:]
 }
